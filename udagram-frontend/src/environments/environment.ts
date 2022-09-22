44059066fc94f581b69c8724c3acd6538e8363fb
AlmeidaJoao <almeidabs.com@gmail.com>
2022-09-23 01:08:07 +0200

    add screenshots

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a9c7b07fe466543159444494ddc59a38-1811974585.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
